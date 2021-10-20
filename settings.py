74104f86c4ecbca211458f7d3f00169e60958483
Sora Youn <sorayoun@Soras-MacBook-Pro.local>
2021-10-20 06:25:47 -0500

    test3

--- settings.py
+++ settings.py
@@ -68,12 +68,12 @@ SESSION_CONFIGS = [
         num_demo_participants=3,
         app_sequence=['Part1', 'Part2','Part3_T2','Part4','Part5','Part6','Part7'],
     ),
-    # dict(
-    #     name='test_T1',
-    #     display_name="test_T1",
-    #     num_demo_participants=3,
-    #     app_sequence=['Part1', 'Part2','Part3_T1_test','Part4','Part5','Part6','Part7'],
-    # ),
+    dict(
+        name='test_T1',
+        display_name="test_T1",
+        num_demo_participants=3,
+        app_sequence=['Part1', 'Part2','Part3_T1_test','Part4','Part5','Part6','Part7'],
+    ),
     # dict(
     #     name='test_T2',
     #     display_name="test_T2",
